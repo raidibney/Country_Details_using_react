572f07df66bd95b9c8f37bee5b5ce54799a97b1c
unknown <youremail@example.com>
2026-03-15 13:31:48 +0600

    new changes

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
     console.log(country.name.common)
-
+    const [visited , setVisited]= useState(false);
     const handleclicked =()=>{
-        console.log('clicked')
+        setVisited(visited?false:true);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'country-visited': 'not-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name:{country.name.common} </h3>
             <p>population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area>300000 ? '(Big Country)':'(Small Country)'}</p>
             <p>Capital:{country.capital.capital}</p>
             <p>Region: {country.region.region}</p>
-            <button onClick={ handleclicked}>Not Visited</button>
+            <button onClick={ handleclicked}>
+                {
+                    visited? 'visited':'not visited'
+                }
+            </button>
         </div>
     );
 };
